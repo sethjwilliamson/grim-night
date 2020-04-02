8cf5d63264ad2a67d6b8cd6be6d3c8b87e437609
SefusJack <seth-jack@outlook.com>
2020-04-01 22:39:30 -0500

    Create Light Wand

---
+++ Grim Night/Assets/player/weapons/Light Weapon/despawnBall.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class despawnBall : MonoBehaviour
+{
+    public Light lt;
+    public float smooth;
+    float timeStamp;
+    // Start is called before the first frame update
+    void Start()
+    {
+        timeStamp = Time.time + 80f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timeStamp <= Time.time)
+            Destroy(gameObject);
+        lt.intensity = Mathf.Lerp(10f, 1f, Time.deltaTime * smooth);
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        timeStamp = timeStamp + 0.5f;
+    }
+}
