c2b0e7ad640890321f94c7e289b8fd6db661f732
SefusJack <seth-jack@outlook.com>
2020-04-03 13:08:38 -0500

    Commence Oof

---
+++ Grim Night/Assets/player/weapons/Damage.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Damage : MonoBehaviour
+{
+    public string type;
+    public float amountOfDamage;
+    public string getType()
+    {
+        return type;
+    }
+    public float getDamage()
+    {
+        return amountOfDamage;
+    }
+}
