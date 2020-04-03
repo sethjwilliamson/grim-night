43ddfe29f17a0cbb23ff1fa56cc3f31d0b6c9e54
SefusJack <seth-jack@outlook.com>
2020-04-03 18:18:22 -0500

    Create kidNapper

--- Grim Night/Assets/enemies/Enemies in the Dark/kidnapper/GetNapped.cs
+++ Grim Night/Assets/enemies/Enemies in the Dark/kidnapper/GetNapped.cs
@@ -4,15 +4,65 @@ using UnityEngine;
 
 public class GetNapped : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float health = 100;
+    public float lightEffectiveness;
+    public float fireEffectiveness;
+    public float waterEffectiveness;
+    public float physicalEffectiveness;
+    public float rangedEffectiveness;
+    private GameObject nabbed;
+    private bool phaseOne;
+    private bool phaseTwo;
+    private bool phaseThree;
+    private bool phaseFour;
+    private Vector3 startPos;
+    public Vector3 checkPoint;
+    private void Start()
     {
-        
+        startPos = transform.root.position;
     }
-
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        
+        nabbed = other.gameObject;
+        nabbed.transform.parent = gameObject.transform;
+        StartCoroutine(Teleport());
+    }
+    private void Update()
+    {
+        if (phaseOne)
+        {
+            nabbed.transform.localPosition = new Vector3(2f, 13f, 0);
+            nabbed.transform.localRotation = Quaternion.Euler(0, 0, 90f);
+            phaseThree = false;
+        }
+        if (phaseTwo)
+        {
+            transform.root.position = checkPoint;
+            nabbed.transform.localPosition = new Vector3(2f, 13f, 0);
+            nabbed.transform.localRotation = Quaternion.Euler(0, 0, 90f);
+            phaseOne = false;
+        }
+        if (phaseThree)
+        {
+            transform.root.position = startPos;
+            phaseTwo = false;
+        }
+    }
+    public IEnumerator Teleport()
+    {
+        phaseOne = true;
+        yield return new WaitForSeconds(1f);
+        phaseTwo = true;
+        yield return new WaitForSeconds(1f);
+        DropEm();
+        yield return new WaitForSeconds(1f);
+        phaseThree = true;
+    }
+    public void DropEm()
+    {
+        nabbed.transform.localRotation = Quaternion.Euler(0, 0, 0);
+        nabbed.transform.parent = null;
+        nabbed = null;
+        phaseTwo = false;
     }
 }
