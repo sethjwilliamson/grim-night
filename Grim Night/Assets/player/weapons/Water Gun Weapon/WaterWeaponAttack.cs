b160b165f1edcef505921e9902d0056374858246
Seth <sethjwilliamson@gmail.com>
2020-04-03 18:54:32 -0500

    Create Water Gun

---
+++ Grim Night/Assets/player/weapons/Water Gun Weapon/WaterWeaponAttack.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterWeaponAttack : MonoBehaviour
+{
+    public ParticleSystem waterSplash;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        //waterSplash = GameObject.Find("water").GetComponent<ParticleSystem>();
+    }
+    
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Instantiate(waterSplash, gameObject.transform.position, gameObject.transform.rotation);
+        }
+    }
+}
