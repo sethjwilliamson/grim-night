f8d388aa5b501f81b8721a2462cc126f24f966cb
Seth <sethjwilliamson@gmail.com>
2020-04-03 23:29:34 -0500

    Update Item Pickup
    Will now destroy any previous item

--- Grim Night/Assets/GetLight.cs
+++ Grim Night/Assets/GetLight.cs
@@ -8,9 +8,20 @@ public class GetLight : MonoBehaviour
 {
     public GameObject prefab;
     public GameObject bone6;
+    public GameObject bone9;
 
     void OnTriggerEnter2D(Collider2D other)
     {
+        foreach (Transform child in bone6.transform)
+        {
+            GameObject.Destroy(child.gameObject);
+        }
+
+        foreach (Transform child in bone9.transform)
+        {
+            GameObject.Destroy(child.gameObject);
+        }
+
         Destroy(gameObject);
         Instantiate(prefab).transform.SetParent(bone6.transform);
     }
