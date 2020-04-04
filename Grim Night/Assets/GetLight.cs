20a09b3f62454c88ae30e16da411f800270d8f79
Seth <sethjwilliamson@gmail.com>
2020-04-03 23:21:24 -0500

    Pickup Weapon Improved
    This script can be used for picking up any weapon instead of just the wand

--- Grim Night/Assets/GetLight.cs
+++ Grim Night/Assets/GetLight.cs
@@ -6,27 +6,12 @@ using UnityEngine;
 
 public class GetLight : MonoBehaviour
 {
-    public GameObject wandPrefab;
-    private GameObject wand;
-
+    public GameObject prefab;
     public GameObject bone6;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     void OnTriggerEnter2D(Collider2D other)
     {
         Destroy(gameObject);
-        Debug.Log("TEST");
-        wand = Instantiate(wandPrefab);
-        wand.transform.SetParent(bone6.transform);
+        Instantiate(prefab).transform.SetParent(bone6.transform);
     }
 }
